0fe137e856e92a4fc84c0531dd0f1693977d7309
tkbec <welcomein1314@gmail.com>
2016-08-29 09:37:54 +0800

    Update 四730.js

--- 四730.js
+++ 四730.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-    $(".time").append("<span>【第一場】9月04日 (四) 19：30</span><br/><span>【第二場】9月11日 (四) 19：30</span><br/><span>【第三場】9月18日 (四) 19：30</span><br/><span>【第四場】8月25日 (四) 19：30</span><br/><span>【其他時間】一對一專業講師諮詢</span>");
+    $(".time").append("<span>【第一場】9月01日 (四) 19：30</span><br/><span>【第二場】9月08日 (四) 19：30</span><br/><span>【第三場】9月15日 (四) 19：30</span><br/><span>【第四場】9月22日 (四) 19：30</span><br/><span>【第五場】9月29日 (四) 19：30</span><br/><span>【其他時間】一對一專業講師諮詢</span>");
 });
